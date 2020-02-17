b3bf9a6a19b6853649b077924aaad3b7a66c7c1e
xiayang1024 <wfy19961024@gmail.com>
2020-02-18 00:44:27 +0800

    内容：增加布局

--- app/src/main/java/com/xiayang/learningforums/MainActivity.java
+++ app/src/main/java/com/xiayang/learningforums/MainActivity.java
@@ -1,14 +1,43 @@
 package com.xiayang.learningforums;
 
-import android.os.Bundle;
-
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayoutMain;
+    NavigationView navMain;
+    Toolbar toolbarMain;
+    TabLayout tabLayoutMain;
+    ViewPager vPMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();  //初始化控件函数
     }
+
+    //  初始化控件函数
+    private void initView() {
+        toolbarMain = findViewById(R.id.main_bar);
+        toolbarMain.setTitle("");
+        setSupportActionBar(toolbarMain);
+        tabLayoutMain = findViewById(R.id.tab_layout);
+        vPMain = findViewById(R.id.view_pager);
+        drawerLayoutMain = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_classification_black_24dp);
+        }
+        navMain = findViewById(R.id.navigation_view);
+    }
+
 }
